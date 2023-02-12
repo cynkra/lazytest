262d6d0c78a17d9fccd9a10ee1a7ee517fcab87b
Kirill Müller <krlmlr+r@mailbox.org>
2023-02-12 11:09:20 +0100

    Copy from happy

---
+++ R/lazytest_local.R
@@ -0,0 +1,141 @@
+#' Test a local source package
+#'
+#' A drop-in replacement for `testthat::test_local()` that by default reruns
+#' only the tests that failed last time.
+#'
+#' @inheritParams testthat::test_local
+#' @param filter Must be `NULL`.
+#' @param lazytest_reset Set to `TRUE` to run all tests, regardless of what the
+#'   last test results were.
+#' @export
+#' @examples
+#' \dontrun{
+#' lazytest_local()
+#' }
+lazytest_local <- function(path = ".",
+                           reporter = NULL,
+                           ...,
+                           lazytest_reset = FALSE,
+                           stop_on_failure = TRUE,
+                           stop_on_warning = FALSE,
+                           filter = NULL,
+                           load_package = "source") {
+
+  # return(test_local(
+  #   path,
+  #   reporter,
+  #   ...,
+  #   stop_on_failure = FALSE,
+  #   stop_on_warning = FALSE,
+  #   filter = filter,
+  #   load_package = load_package
+  # ))
+
+  if (!is.null(filter)) {
+    cli::cli_abort("{.code lazytest_local()} requires {.code filter = NULL}, use {.code testthat::test_local()} to pass a {.code filter} argument.")
+  }
+
+  CONTEXT_FILE_NAME <- ".happy"
+
+  has_context <- fs::file_exists(CONTEXT_FILE_NAME)
+  if (has_context) {
+    contexts <- brio::read_lines(CONTEXT_FILE_NAME)
+    fs::file_delete(CONTEXT_FILE_NAME)
+  }
+
+  if (!has_context || lazytest_reset) {
+    contexts <- NULL
+  }
+
+  if (is.null(contexts)) {
+    cli::cli_inform(c(
+      "i" = "Testing all tests."
+    ))
+  } else {
+    cli::cli_inform(c(
+      "i" = "Testing only tests that failed last time: ",
+      set_names(contexts, "*")
+    ))
+
+    filter <- rex::rex(
+      start,
+      or(contexts),
+      end
+    )
+  }
+
+  orig_reporter <- reporter
+
+  want_parallel <- find_parallel(path, load_package, package)
+
+  if (is.null(reporter)) {
+    if (want_parallel) {
+      reporter <- default_parallel_reporter()
+    } else {
+      reporter <- default_reporter()
+    }
+  }
+  reporters <- find_reporter_list(reporter)
+
+  my_list_reporter <- ListReporter$new()
+
+  reporter <- MultiReporter$new(c(reporters, list(my_list_reporter)))
+
+  out <- test_local(
+    path,
+    reporter,
+    ...,
+    stop_on_failure = FALSE,
+    stop_on_warning = FALSE,
+    filter = filter,
+    load_package = load_package
+  )
+
+  result_df <- as.data.frame.testthat_results(my_list_reporter$get_results())
+
+  passed_idx <- result_df$failed == 0 & !result_df$error
+
+  failed_files <- unique(result_df$file[!passed_idx])
+
+  failed_contexts <- context_name(failed_files)
+
+  if (length(failed_contexts) > 0) {
+    brio::write_lines(failed_contexts, CONTEXT_FILE_NAME)
+    if (identical(failed_contexts, contexts)) {
+      cli::cli_inform(c(
+        ">" = "Repeating the same tests next time."
+      ))
+    } else {
+      cli::cli_inform(c(
+        ">" = "Testing the following tests next time: ",
+        set_names(failed_contexts, "*")
+      ))
+    }
+  } else if (has_context) {
+    cli::cli_inform(c(
+      ">" = "Testing all tests now."
+    ))
+
+    return(lazytest_local(
+      path,
+      orig_reporter,
+      ...,
+      stop_on_failure = FALSE,
+      stop_on_warning = FALSE,
+      filter = NULL,
+      load_package = load_package
+    ))
+  } else {
+    cli::cli_inform(c(
+      ">" = "Testing all tests next time."
+    ))
+  }
+
+  test_files_check_df(
+    result_df,
+    stop_on_failure = stop_on_failure,
+    stop_on_warning = stop_on_warning
+  )
+
+  invisible(my_list_reporter$get_results())
+}
---
+++ R/testthat.R
@@ -0,0 +1,173 @@
+# From testthat, MIT licensed, https://testthat.r-lib.org/LICENSE.html
+
+find_parallel <- function(path, load_package = "source", package = NULL) {
+  # If env var is set, then use that
+  parenv <- Sys.getenv("TESTTHAT_PARALLEL", NA_character_)
+  if (!is.na(parenv)) {
+    if (toupper(parenv) == "TRUE") return(TRUE)
+    if (toupper(parenv) == "FALSE") return(FALSE)
+    abort("`TESTTHAT_PARALLEL` must be `TRUE` or `FALSE`")
+  }
+
+  # Make sure we get the local package package if not "installed"
+  if (load_package != "installed") package <- NULL
+  desc <- find_description(path, package)
+  if (is.null(desc)) {
+    return(FALSE)
+  }
+
+  par <- identical(
+    toupper(desc$get_field("Config/testthat/parallel", default = "FALSE")),
+    "TRUE"
+  )
+
+  if (par) {
+    ed <- as.integer(desc$get_field("Config/testthat/edition", default = 2L))
+    if (ed < 3) {
+      inform("Running tests in parallel requires the 3rd edition")
+      par <- FALSE
+    }
+  }
+
+  par
+}
+
+find_description <- function(path, package = NULL) {
+  if (!is.null(package)) {
+    return(desc::desc(package = package))
+  } else {
+    tryCatch(
+      pkgload::pkg_desc(path),
+      error = function(e) NULL
+    )
+  }
+}
+
+#' Find reporter object given name or object.
+#'
+#' If not found, will return informative error message.
+#' Pass a character vector to create a [MultiReporter] composed
+#' of individual reporters.
+#' Will return null if given NULL.
+#'
+#' @param reporter name of reporter(s), or reporter object(s)
+#' @noRd
+#' @keywords internal
+find_reporter_list <- function(reporter) {
+  if (is.null(reporter)) return(NULL)
+
+  if (inherits(reporter, "R6ClassGenerator")) {
+    list(reporter$new())
+  } else if (inherits(reporter, "Reporter")) {
+    list(reporter)
+  } else if (is.character(reporter)) {
+    if (length(reporter) <= 1L) {
+      list(find_reporter_one(reporter))
+    } else {
+      lapply(reporter, find_reporter_one)
+    }
+  } else {
+    stop("Invalid input", call. = FALSE)
+  }
+}
+
+find_reporter_one <- function(reporter, ...) {
+  stopifnot(is.character(reporter))
+
+  name <- reporter
+  substr(name, 1, 1) <- toupper(substr(name, 1, 1))
+  name <- paste0(name, "Reporter")
+
+  if (!exists(name)) {
+    stop("Can not find test reporter ", reporter, call. = FALSE)
+  }
+
+  get(name)$new(...)
+}
+
+test_files_check_df <- function(df, stop_on_failure = TRUE, stop_on_warning = FALSE) {
+  if (stop_on_failure && !all_passed_df(df)) {
+    stop("Test failures", call. = FALSE)
+  }
+  if (stop_on_warning && any_warnings_df(df)) {
+    stop("Tests generated warnings", call. = FALSE)
+  }
+
+  invisible(df)
+}
+
+# return if all tests are successful w/o error
+all_passed_df <- function(df) {
+  sum(df$failed) == 0 && all(!df$error)
+}
+
+any_warnings_df <- function(df) {
+  any(df$warning > 0)
+}
+
+as.data.frame.testthat_results <- function(x, ...) {
+  if (length(x) == 0) {
+    return(
+      data.frame(
+        file = character(0), context = character(0), test = character(0),
+        nb = integer(0), failed = integer(0), skipped = logical(0),
+        error = logical(0), warning = integer(0),
+        user = numeric(0), system = numeric(0), real = numeric(0),
+        passed = integer(0), result = list(),
+        stringsAsFactors = FALSE
+      )
+    )
+  }
+
+  rows <- lapply(x, summarize_one_test_results)
+  do.call(rbind, rows)
+}
+
+summarize_one_test_results <- function(test) {
+  test_results <- test$results
+  nb_tests <- length(test_results)
+
+  nb_failed <- nb_skipped <- nb_warning <- nb_passed <- 0L
+  error <- FALSE
+
+  if (nb_tests > 0) {
+    # error reports should be handled differently.
+    # They may not correspond to an expect_that() test so remove them
+    last_test <- test_results[[nb_tests]]
+    error <- expectation_error(last_test)
+    if (error) {
+      test_results <- test_results[-nb_tests]
+      nb_tests <- length(test_results)
+    }
+
+    nb_passed <- sum(vapply(test_results, expectation_success, logical(1)))
+    nb_skipped <- sum(vapply(test_results, expectation_skip, logical(1)))
+    nb_failed <- sum(vapply(test_results, expectation_failure, logical(1)))
+    nb_warning <- sum(vapply(test_results, expectation_warning, logical(1)))
+  }
+
+  context <- if (length(test$context) > 0) test$context else ""
+
+  res <- data.frame(
+    file = test$file, context = context, test = test$test,
+    nb = nb_tests, failed = nb_failed, skipped = as.logical(nb_skipped),
+    error = error, warning = nb_warning,
+    user = test$user, system = test$system, real = test$real,
+    stringsAsFactors = FALSE
+  )
+
+  # Added at end for backward compatibility
+  res$passed <- nb_passed
+
+  # Cannot easily add list columns in data.frame()
+  res$result <- list(test_results)
+  res
+}
+
+context_name <- function(filename) {
+  # Remove test- prefix
+  filename <- sub("^test[-_]", "", filename)
+  # Remove terminal extension
+  filename <- sub("[.][Rr]$", "", filename)
+  filename
+}
