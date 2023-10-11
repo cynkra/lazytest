e54324328fd508f59a87394470d88f43da72d220
Kirill Müller <kirill@cynkra.com>
2023-10-11 09:38:17 +0200

    chore: Use nice to run tests

--- R/addins.R
+++ R/addins.R
@@ -1,17 +1,29 @@
 run_tests <- function() {
   rscript <- paste0("'", file.path(R.home("bin"), "Rscript"), "' ")
-  cmd <- paste0(rscript, "-e 'testthat::test_local()'")
+  nice <- Sys.which("nice")
+  if (nice != "") {
+    nice <- paste0("'", nice, "' ")
+  }
+  cmd <- paste0(nice, rscript, "-e 'testthat::test_local()'")
   rstudioapi::terminalExecute(cmd)
 }
 
 run_lazytests <- function() {
   rscript <- paste0("'", file.path(R.home("bin"), "Rscript"), "' ")
-  cmd <- paste0(rscript, "-e '", "lazytest::lazytest_local()'")
+  nice <- Sys.which("nice")
+  if (nice != "") {
+    nice <- paste0("'", nice, "' ")
+  }
+  cmd <- paste0(nice, rscript, "-e '", "lazytest::lazytest_local()'")
   rstudioapi::terminalExecute(cmd)
 }
 
 run_lazytests_reset <- function() {
   rscript <- paste0("'", file.path(R.home("bin"), "Rscript"), "' ")
-  cmd <- paste0(rscript, "-e '", "lazytest::lazytest_local(lazytest_reset = TRUE)'")
+  nice <- Sys.which("nice")
+  if (nice != "") {
+    nice <- paste0("'", nice, "' ")
+  }
+  cmd <- paste0(nice, rscript, "-e '", "lazytest::lazytest_local(lazytest_reset = TRUE)'")
   rstudioapi::terminalExecute(cmd)
 }
